d72b16ffc8d83cf1fa701715c1f1cd1cf6685163
Alexander Kleinen <kornhammer@gmail.com>
2018-01-29 14:02:08 +0100

    make it so

--- lib/base.py
+++ lib/base.py
@@ -1,7 +1,27 @@
+# Simple demo of of the WS2801/SPI-like addressable RGB LED lights.
+import time
+import RPi.GPIO as GPIO
+
+# Import the WS2801 module.
+import Adafruit_WS2801
+import Adafruit_GPIO.SPI as SPI
+
+# Configure the count of pixels:
+PIXEL_COUNT = 160
+
+# Alternatively specify a hardware SPI connection on /dev/spidev0.0:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+
+led_stripe = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE), gpio=GPIO)
+
 class Base:
+
+    # led_stripe
+    # led_count
+    #
+    PIXEL_COUNT = 160
 
     def __init__(self):
         print("init base")
-
-    def xx(self):
-        print("xxxxx")
+        # self.led_count = PIXEL_COUNT
