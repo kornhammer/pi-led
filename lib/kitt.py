5f95804795d1db4774eb26e42265d64006310837
Alexander Kleinen <kornhammer@gmail.com>
2019-06-24 22:38:43 +0200

    some stuff

--- lib/kitt.py
+++ lib/kitt.py
@@ -1,5 +1,6 @@
 from lib.base import Base
 
+
 class Kitt(Base):
     pass
 
@@ -8,4 +9,22 @@ class Kitt(Base):
         print("init kitt")
 
     def run(self):
-        print("i will run")
+        # Clear all the pixels to turn them off.
+        pixels.clear()
+        pixels.show()  # Make sure to call show() after changing any pixels!
+
+        rainbow_cycle_successive(pixels, wait=0.1)
+        rainbow_cycle(pixels, wait=0.01)
+
+        brightness_decrease(pixels)
+
+        appear_from_back(pixels)
+
+        for i in range(3):
+            blink_color(pixels, blink_times=1, color=(255, 0, 0))
+            blink_color(pixels, blink_times=1, color=(0, 255, 0))
+            blink_color(pixels, blink_times=1, color=(0, 0, 255))
+
+        rainbow_colors(pixels)
+
+        brightness_decrease(pixels)
